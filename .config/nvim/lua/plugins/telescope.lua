0437c83a7a04d9841c02a4ecb3d11ccae8359560
Jonathan Fesseha <jonathan.fesseha@student.manchester.ac.uk>
2025-02-12 01:30:01 +0000

    telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,18 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>pf", function()
 			builtin.find_files({
-				find_command = { "rg", "--files", "--iglob", "!.git", "--iglob", "!**/venv/*", "--hidden" },
+				find_command = {
+					"rg",
+					"--files",
+					"--iglob",
+					"!.git",
+					"--iglob",
+					"!**/venv/*",
+					"!**/build/*",
+					"!**/target/*",
+					"!/dist/*",
+					"--hidden",
+				},
 			})
 		end, { desc = "Search files" })
 		vim.keymap.set("n", "<leader>pc", function()
