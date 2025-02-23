cffdf762a7f7dac03f2d1dae288a52857aebaef7
Jonathan Fesseha <jonathan.fesseha@student.manchester.ac.uk>
2025-02-23 21:26:51 +0000

    flash + suround change

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,12 +1,33 @@
 return {
-	"tpope/vim-surround",
+	"kylechui/nvim-surround",
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
 	config = function()
-		-- Add descriptions for vim-surround mappings
-		vim.keymap.set("n", "ds", "<Plug>Dsurround", { desc = "Delete surrounding characters" })
-		vim.keymap.set("n", "cs", "<Plug>Csurround", { desc = "Change surrounding characters" })
-		vim.keymap.set("n", "ys", "<Plug>Ysurround", { desc = "Add surrounding characters" })
-		vim.keymap.set("n", "yss", "<Plug>Yssurround", { desc = "Add surrounding characters (whole line)" })
-		vim.keymap.set("n", "ySs", "<Plug>YSsurround", { desc = "Add surrounding characters (whole line, line mode)" })
-		vim.keymap.set("n", "ySS", "<Plug>YSsurround", { desc = "Add surrounding characters (whole line, line mode)" })
+		require("nvim-surround").setup({
+			-- Configuration here, or leave empty to use defaults
+			keymaps = {
+				insert = "<C-g>s",
+				insert_line = "<C-g>S",
+				normal = "ys",
+				normal_cur = "yss",
+				normal_line = "yS",
+				normal_cur_line = "ySS",
+				visual = "S",
+				visual_line = "gS",
+				delete = "d<leader>s",
+				change = "c<leader>s",
+				change_line = "mcS",
+			},
+		})
 	end,
 }
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
