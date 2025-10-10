1bed5ff034d7d3768d973d064c5fddb7c4c5a225
Jonathan Fesseha <jonathan.fesseha@student.manchester.ac.uk>
2025-10-10 23:16:57 +0100

    updates

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,10 @@
 return {
-	"github/copilot.vim",
-	config = function()
-		local opts = { expr = true, silent = true }
-
-		vim.keymap.set("i", "<C-J>", "copilot#Previous()", opts) -- Previous suggestion
-		vim.keymap.set("i", "<C-K>", "copilot#Next()", opts) -- Next suggestion
-	end,
+	-- "github/copilot.vim",
+	-- config = function()
+	-- 	local opts = { expr = true, silent = true }
+	--
+	-- 	vim.keymap.set("i", "<C-J>", "copilot#Previous()", opts) -- Previous suggestion
+	-- 	vim.keymap.set("i", "<C-K>", "copilot#Next()", opts) -- Next suggestion
+	-- end,
 }
 -- tab to complete
